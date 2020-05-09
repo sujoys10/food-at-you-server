8f12efce818855c125d4231efc0146377536ef96
Sujoy Saha <sujoys023@gmail.com>
2020-05-09 13:28:55 +0530

    added prisma server

--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -44,6 +44,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `http://192.168.99.100:4466/`
+  endpoint: `https://fay-server-8bf829119e.herokuapp.com/`
 });
 exports.prisma = new exports.Prisma();
